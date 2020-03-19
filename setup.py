00f561c5422fdde16b101c59b9921d496ba77dd0
mammo0 <marc.ammon@hotmail.de>
2020-03-19 15:35:19 +0100

    fixed license classifier

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=["simple_plugin_loader"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
